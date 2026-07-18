f9695e71bf8aa74ccf93e207a2ee6f0774918a7f
Deardemon <dear.dan3796@gmail.com>
2026-07-18 19:56:37 +0800

    Change App function to a named function

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ async function dbUpsertVisit(dateStr, customer, agent) {
 /* ============================================================
    App Root
 ============================================================ */
-export default function App() {
+function App() {
   const [view, setView] = useState({ name: "home" });
   const [agents, setAgents] = useState(null);
   const [cases, setCases] = useState(null); // 案件池
